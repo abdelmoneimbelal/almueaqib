815e90364f69b4192575f45767b3f6090b4f4dce
abdelmoneimbelal <abdobelal069@gmail.com>
2019-04-11 18:34:49 +0200

    first commit

---
+++ resources/views/layouts/dashboard/_aside.blade.php
@@ -0,0 +1,111 @@
+<aside class="main-sidebar">
+
+    <section class="sidebar">
+
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="{{ asset('dashboard/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
+            </div>
+            <div class="pull-left info">
+            <p> {{auth()->user()->name}} </p>
+        <!-- <a href="{{auth()->user()->name}}" style='color:#fff'></i> {{auth()->user()->name}}</a> -->
+            </div>
+        </div>
+
+        <ul class="sidebar-menu" data-widget="tree">
+            <li><a href="{{ route('dashboard.index') }}"><i class="fa fa-th"></i><span>@lang('site.dashboard')</span></a></li>
+
+            @if (auth()->user()->hasPermission('read_categories'))
+                <li><a href="{{ route('dashboard.categories.index') }}"><i class="fa fa-th"></i><span>@lang('site.categories')</span></a></li>
+            @endif
+            <li><a href="{{ route('dashboard.mangs.index') }}"><i class="fa fa-th"></i><span>الإدارات الملحقة</span></a></li>
+
+
+            @if (auth()->user()->hasPermission('read_products'))
+                <li><a href="{{ route('dashboard.products.index') }}"><i class="fa fa-book"></i><span>@lang('site.products')</span></a></li>
+            @endif
+
+            @if (auth()->user()->hasPermission('read_clients'))
+                <li><a href="{{ route('dashboard.clients.index') }}"><i class="fa fa-th"></i><span>@lang('site.clients')</span></a></li>
+            @endif
+
+            @if (auth()->user()->hasPermission('read_users'))
+                <li><a href="{{ route('dashboard.users.index') }}"><i class="fa fa-th"></i><span>@lang('site.users')</span></a></li>
+            @endif
+
+            {{--<li><a href="{{ route('dashboard.categories.index') }}"><i class="fa fa-book"></i><span>@lang('site.categories')</span></a></li>--}}
+            {{----}}
+            {{----}}
+
+
+
+             @if (auth()->user()->hasPermission('read_orders'))
+             <li><a href="{{ route('dashboard.orders.index') }}"><i class="fa fa-th"></i><span>المعاملات</span></a></li>
+            @endif
+              @if (auth()->user()->hasPermission('read_reports'))
+             <li><a href="{{ route('dashboard.reports.index') }}"><i class="fa fa-book"></i><span> التقارير</span></a></li>
+            @endif
+             @if (auth()->user()->hasPermission('read_records'))
+             <li><a href="{{ route('dashboard.records.index') }}"><i class="fa fa-th"></i><span>سجل الإدارات</span></a></li>
+            @endif
+             @if (auth()->user()->hasPermission('read_baptists'))
+             <li><a href="{{ route('dashboard.baptists.index') }}"><i class="fa fa-th"></i><span>المعمد والمنفذ</span></a></li>
+            @endif
+             @if (auth()->user()->hasPermission('read_externals'))
+             <li><a href="{{ route('dashboard.external.index') }}"><i class="fa fa-th"></i><spa>المنفذ الخارجى</span></a></li>
+            @endif
+              @if (auth()->user()->hasPermission('read_offecs'))
+             <li><a href="{{ route('dashboard.offecs.index') }}"><i class="fa fa-book"></i><spa>مكاتب المعقبين</span></a></li>
+            @endif
+
+             @if (auth()->user()->hasPermission('read_catchs'))
+             <li><a href="{{ route('dashboard.catchs.index') }}"><i class="fa fa-book"></i><span>سند القبض </span></a></li>
+            @endif
+
+             {{--@if (auth()->user()->hasPermission('read_receipts'))--}}
+
+             <li><a href="{{ route('dashboard.receipts.index') }}"><i class="fa fa-book"></i><span>سند  الصرف</span></a></li>
+            {{--@endif--}}
+
+
+            {{----}}
+            {{----}}
+
+           
+
+            {{----}}
+            {{----}}
+
+
+
+            {{--<li><a href="{{ route('dashboard.users.index') }}"><i class="fa fa-users"></i><span>@lang('site.users')</span></a></li>--}}
+
+            {{--<li class="treeview">--}}
+            {{--<a href="#">--}}
+            {{--<i class="fa fa-pie-chart"></i>--}}
+            {{--<span>الخرائط</span>--}}
+            {{--<span class="pull-right-container">--}}
+            {{--<i class="fa fa-angle-left pull-right"></i>--}}
+            {{--</span>--}}
+            {{--</a>--}}
+            {{--<ul class="treeview-menu">--}}
+            {{--<li>--}}
+            {{--<a href="../charts/chartjs.html"><i class="fa fa-circle-o"></i> ChartJS</a>--}}
+            {{--</li>--}}
+            {{--<li>--}}
+            {{--<a href="../charts/morris.html"><i class="fa fa-circle-o"></i> Morris</a>--}}
+            {{--</li>--}}
+            {{--<li>--}}
+            {{--<a href="../charts/flot.html"><i class="fa fa-circle-o"></i> Flot</a>--}}
+            {{--</li>--}}
+            {{--<li>--}}
+            {{--<a href="../charts/inline.html"><i class="fa fa-circle-o"></i> Inline charts</a>--}}
+            {{--</li>--}}
+            {{--</ul>--}}
+            {{--</li>--}}
+        </ul>
+
+    </section>
+
+</aside>
+
